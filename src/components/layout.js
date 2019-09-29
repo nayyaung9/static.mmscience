095c87a381db5b7c83b53a44fb61e46305018b0b
nayyaung9 <naygit.developer@gmail.com>
2019-09-29 12:58:12 +0630

    Install required plugins

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,52 +1,37 @@
-/**
- * Layout component that queries for data
- * with Gatsby's useStaticQuery component
- *
- * See: https://www.gatsbyjs.org/docs/use-static-query/
- */
-
 import React from "react"
-import PropTypes from "prop-types"
-import { useStaticQuery, graphql } from "gatsby"
+import { css } from "@emotion/core"
+import { Link } from "gatsby"
 
-import Header from "./header"
-import "./layout.css"
+import { rhythm } from "../utils/typography"
 
-const Layout = ({ children }) => {
-  const data = useStaticQuery(graphql`
-    query SiteTitleQuery {
-      site {
-        siteMetadata {
-          title
-        }
-      }
-    }
-  `)
-
-  return (
-    <>
-      <Header siteTitle={data.site.siteMetadata.title} />
-      <div
-        style={{
-          margin: `0 auto`,
-          maxWidth: 960,
-          padding: `0px 1.0875rem 1.45rem`,
-          paddingTop: 0,
-        }}
+export default ({ children }) => (
+  <div
+    css={css`
+      margin: 0 auto;
+      max-width: 700px;
+      padding: ${rhythm(2)};
+      padding-top: ${rhythm(1.5)};
+    `}
+  >
+    <Link to={`/`}>
+      <h3
+        css={css`
+          margin-bottom: ${rhythm(2)};
+          display: inline-block;
+          font-style: normal;
+        `}
       >
-        <main>{children}</main>
-        <footer>
-          © {new Date().getFullYear()}, Built with
-          {` `}
-          <a href="https://www.gatsbyjs.org">Gatsby</a>
-        </footer>
-      </div>
-    </>
-  )
-}
-
-Layout.propTypes = {
-  children: PropTypes.node.isRequired,
-}
-
-export default Layout
+        Pandas Eating Lots
+      </h3>
+    </Link>
+    <Link
+      to={`/about/`}
+      css={css`
+        float: right;
+      `}
+    >
+      About
+    </Link>
+    {children}
+  </div>
+)
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,21 +1,14 @@
 import React from "react"
-import { Link } from "gatsby"
-
 import Layout from "../components/layout"
-import Image from "../components/image"
-import SEO from "../components/seo"
 
-const IndexPage = () => (
+export default () => (
   <Layout>
-    <SEO title="Home" />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-      <Image />
+    <h1>Amazing Pandas Eating Things</h1>
+    <div>
+      <img
+        src="https://2.bp.blogspot.com/-BMP2l6Hwvp4/TiAxeGx4CTI/AAAAAAAAD_M/XlC_mY3SoEw/s1600/panda-group-eating-bamboo.jpg"
+        alt="Group of pandas eating bamboo"
+      />
     </div>
-    <Link to="/page-2/">Go to page 2</Link>
   </Layout>
 )
-
-export default IndexPage
