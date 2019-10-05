2baf7fdf161c8af983d966c86d64342d97abbbb4
nayyaung9 <naygit.developer@gmail.com>
2019-10-05 16:04:23 +0630

    Updates

---
+++ component---src-pages-mmscience-js-1841d0e5e75ea5ea43ad.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[7],{237:function(n,c,e){"use strict";e.r(c);var t=e(3),u=(e(1),e(230));c.default=function(){return Object(t.b)(u.a,null,Object(t.b)("h1",null,"MM Science"))}}}]);
+//# sourceMappingURL=component---src-pages-mmscience-js-1841d0e5e75ea5ea43ad.js.map
