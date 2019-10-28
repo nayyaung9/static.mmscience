82d861663051abba0d9c945ef415be0c3dd40c6d
nayyaung9 <naygit.developer@gmail.com>
2019-10-28 22:01:15 +0630

    create SEO component

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,6 +2,10 @@ module.exports = {
   pathPrefix: "/mmscience",
   siteMetadata: {
     title: `MM Science`,
+    description:
+      "MMScience is a online library website for Myanmar High School Students.",
+    url: "https://mmscience-2019.firebaseapp.com", // No trailing slash allowed!
+    image: "./src/assets/mmscience0.jpg", // Path to your image you placed in the 'static' folder
   },
   plugins: [
     {
